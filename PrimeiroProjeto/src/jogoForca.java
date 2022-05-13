5f3a1f2765ac40b0f6a2d384511302a956bcef2a
Felipe Getulio Pereira <felipegetulio@furb.br>
2022-05-13 18:47:20 -0300

    jogo

--- PrimeiroProjeto/src/jogoForca.java
+++ PrimeiroProjeto/src/jogoForca.java
@@ -1,40 +1,295 @@
-import javax.swing.*;
-import java.util.Locale;
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.util.Scanner;
 public class jogoForca {
 
     public static void main(String[] args) {
-        Palavra();
-    }
 
-    public static void Palavra() {
+        //pega a palavra
         Scanner ler = new Scanner(System.in);
-        System.out.println("Informe a palavra para o jogo: ");
+        System.out.println("●▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬●");
+        System.out.println("░░░░░░░░░░░! ! Jogo Da Forca! !░░░░░░░░░░░ ");
+        System.out.println("●▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬●\n");
+        System.out.println(" ≪ ━─ ━ ─ ━ ─ ━ ─ ◈ ─ ━ ─ ━ ─ ━ ─ ━ ≫");
+        System.out.print("    Informe a palavra sem acento para o jogo: ");
         String palavraDesconhecida = ler.nextLine().toUpperCase();
 
+        //limpa o console
         for (int i = 0; i < 100; i++) {
-            System.out.print("");
+            System.out.print("\n");
         }
 
-        boolean continua = true;
-        int tamanho = palavraDesconhecida.length();
-        char[] letras = palavraDesconhecida.toCharArray();
+        // Controla os acertos do usuário, coloca todos caracter como 0 e se acertar vira 1.
+        char[] acertos = new char[palavraDesconhecida.length()];
+        for (int i = 0; i < acertos.length; i++) {
+            acertos[i] = 0;
+        }
 
-        while(continua) {
-            System.out.println("Informe uma letra: ");
-            String letraInformada = ler.next();
+        //Declaração de variaveis
+        int chances = 6;
+        char letra;
+        String letrasUsadas ="";
+        char [] numeros = {'0', '1', '2', '3', '4', '5', '6','7', '8', '9'};
 
-            for (int i = 0; i < tamanho; i++) {
-                if (letraInformada.charAt(0) == letras[i]){
-                    System.out.println("Você acertou uma letra");
 
+
+        // Quantidade de underlines por letra da palavra sorteada
+        for (int i = 0; i < palavraDesconhecida.length(); i++) {
+            if(palavraDesconhecida.charAt(i) == ' ') {
+                System.out.print("   ");
+            } else {
+                System.out.print(" _ ");
+            }
+        }
+
+        // Declaração do "acertou" fora como boolean = false para poder colocar dentro do while como condição de parada
+        boolean acertou = false;
+
+
+        // começando o jogo
+        while (!acertou && chances > 0) {
+
+
+            System.out.print("꧁ Informe uma letra: ");
+            letra = ler.next().toUpperCase().charAt(0);
+
+
+
+            //verificar se a leta ja foi utilizada
+            if (letrasUsadas.indexOf(letra) >= 0) {
+                System.out.printf("Você já tentou a letra %c.%n", letra);
+                for (int i = 0; i < palavraDesconhecida.length(); i++) {
+
+                    if(palavraDesconhecida.charAt(i) == ' ') {
+                        System.out.print("   ");
+                    } else if (acertos[i] == 0) {
+                        System.out.print(" _ ");
+                    } else {
+                        System.out.print(" " + palavraDesconhecida.charAt(i) + " ");
+                    }
+                }
+                continue;
+            } else {
+                letrasUsadas += letra;
+            }
+            //Para não aceitar números
+            for (char numero : numeros) {
+                if (letra == numero) {
+                    System.out.println("\nNúmeros não são letras! Tentou achar um bug e perdeu uma vida ;)");
                     break;
                 }
             }
+            // Controlador de vidas
+            boolean vida = true;
+            for (int i = 0; i < acertos.length; i++) {
 
-            continua = !continua;
+                if (letra == palavraDesconhecida.charAt(i)) { // Executa se acertar a letra
+                    acertos[i] = 1;
+                    vida = false;
+                }
+            }
+            if (vida) { // Executa se errar a letra
+                chances--;
+                if (chances == 5) {
+                    System.out.println("\n");
+                    System.out.println("▃▅▆█ 웃 █▆▅▃");
+                    System.out.println("_____");
+                    System.out.println("|    |");
+                    System.out.println("|  .___.");
+                    System.out.println("|  |◕‿◕|");
+                    System.out.println("|  |___| ");
+                    System.out.println("|");
+                    System.out.println("|");
+                    System.out.println("|");
+                    System.out.println("|");
+                    System.out.println("|");
+                    System.out.println("|");
+                    System.out.println("Você me colocou na forca, temos mais: " + chances + " chances \n");
+                    for (int i = 0; i < palavraDesconhecida.length(); i++) {
+
+                        if(palavraDesconhecida.charAt(i) == ' ') {
+                            System.out.print("   ");
+                        } else if (acertos[i] == 0) {
+                            System.out.print(" _ ");
+                        } else {
+                            System.out.print(" " + palavraDesconhecida.charAt(i) + " ");
+                        }
+                    }
+                    continue;
+                } else if (chances == 4) {
+                    System.out.println("\n");
+                    System.out.println("▃▅▆█ 웃 █▆▅▃");
+                    System.out.println("_____");
+                    System.out.println("|    |");
+                    System.out.println("|  .___.");
+                    System.out.println("|  |●▂●|");
+                    System.out.println("|  |___| ");
+                    System.out.println("|    |");
+                    System.out.println("|    |");
+                    System.out.println("|    |");
+                    System.out.println("|    |");
+                    System.out.println("|");
+                    System.out.println("|");
+                    System.out.println("Pensa direito, meu corpo ja esta na forca: " + chances + " chances \n");
+                    for (int i = 0; i < palavraDesconhecida.length(); i++) {
+
+                        if(palavraDesconhecida.charAt(i) == ' ') {
+                            System.out.print("   ");
+                        } else if (acertos[i] == 0) {
+                            System.out.print(" _ ");
+                        } else {
+                            System.out.print(" " + palavraDesconhecida.charAt(i) + " ");
+                        }
+                    }
+                    continue;
+                } else if (chances == 3) {
+                    System.out.println("\n");
+                    System.out.println("▃▅▆█ 웃 █▆▅▃");
+                    System.out.println("_____");
+                    System.out.println("|    |");
+                    System.out.println("|  .___.");
+                    System.out.println("|ب_ب|  |");
+                    System.out.println("|  |___| ");
+                    System.out.println("|    |");
+                    System.out.println("|    |");
+                    System.out.println("|    |");
+                    System.out.println("|    |");
+                    System.out.println("|     \\");
+                    System.out.println("|      \\");
+                    System.out.println("Ai minha perninha: " + chances + " chances \n");
+                    for (int i = 0; i < palavraDesconhecida.length(); i++) {
+
+                        if(palavraDesconhecida.charAt(i) == ' ') {
+                            System.out.print("   ");
+                        } else if (acertos[i] == 0) {
+                            System.out.print(" _ ");
+                        } else {
+                            System.out.print(" " + palavraDesconhecida.charAt(i) + " ");
+                        }
+                    }
+                    continue;
+                }else if (chances == 2) {
+                    System.out.println("\n");
+                    System.out.println("▃▅▆█ 웃 █▆▅▃");
+                    System.out.println("_____");
+                    System.out.println("|    |");
+                    System.out.println("|  .____.");
+                    System.out.println("|  |ಠ╭╮ಠ|");
+                    System.out.println("|  |_____| ");
+                    System.out.println("|    |");
+                    System.out.println("|    |");
+                    System.out.println("|    |");
+                    System.out.println("|    |");
+                    System.out.println("|   / \\");
+                    System.out.println("|  /   \\");
+                    System.out.println("Desse jeito você vai me matar: " + chances + " chances \n");
+                    for (int i = 0; i < palavraDesconhecida.length(); i++) {
+
+                        if(palavraDesconhecida.charAt(i) == ' ') {
+                            System.out.print("   ");
+                        } else if (acertos[i] == 0) {
+                            System.out.print(" _ ");
+                        } else {
+                            System.out.print(" " + palavraDesconhecida.charAt(i) + " ");
+                        }
+                    }
+                    continue;
+                }else if (chances == 1) {
+                    System.out.println("\n");
+                    System.out.println("▃▅▆█ 웃 █▆▅▃");
+                    System.out.println("_____");
+                    System.out.println("|    |");
+                    System.out.println("|  .___.");
+                    System.out.println("|  |╥_╥|");
+                    System.out.println("|  |___| ");
+                    System.out.println("|    |");
+                    System.out.println("|    | /");
+                    System.out.println("|    |/");
+                    System.out.println("|    |");
+                    System.out.println("|   / \\");
+                    System.out.println("|  /   \\");
+                    System.out.println("Essa é nossa ultima chance, pensa com carinho: " + chances + " chance \n");
+                    for (int i = 0; i < palavraDesconhecida.length(); i++) {
+
+                        if(palavraDesconhecida.charAt(i) == ' ') {
+                            System.out.print("   ");
+                        } else if (acertos[i] == 0) {
+                            System.out.print(" _ ");
+                        } else {
+                            System.out.print(" " + palavraDesconhecida.charAt(i) + " ");
+                        }
+                    }
+                    continue;
+
+                }else if (chances == 0) {
+                    System.out.println("\n");
+                    System.out.println("▃▅▆█ 웃 █▆▅▃");
+                    System.out.println("_____");
+                    System.out.println("|    |");
+                    System.out.println("| ._____.");
+                    System.out.println("| |✖╭╮✖|");
+                    System.out.println("| |_____| ");
+                    System.out.println("|    |");
+                    System.out.println("|  \\ | /");
+                    System.out.println("|   \\|/");
+                    System.out.println("|    |");
+                    System.out.println("|   / \\");
+                    System.out.println("|  /   \\");
+                    break;
+                }
+
+            }
+
+            // Controlador de acertos
+            acertou = true;
+            for (int i = 0; i < palavraDesconhecida.length(); i++) {
+
+                if(palavraDesconhecida.charAt(i) == ' ') {
+                    System.out.print("   ");
+                } else if (acertos[i] == 0) { // Se não acertar entra na condição imprime o underline e false pra acerto
+                    System.out.print(" _ ");
+                    acertou = false;
+                } else { //se acertar a letra imprime o caracter da palavra sorteada
+                    System.out.print(" " + palavraDesconhecida.charAt(i) + " ");
+                }
+            }
+
         }
 
+        //Imprime o resultado final, de qualquer forma o YanDev tem que morrer.
+        if(chances != 0) {
+            System.out.println("\n");
+            System.out.println("   ▬▬▬.◙.▬▬▬");
+            System.out.println("     .▂▄▄▓▄▄▂");
+            System.out.println("   ◢◤ █▀▀████▄▄▄▄▄▄◢◤");
+            System.out.println("   |█▄ █ :) ██▀▀▀▀▀▀▀╬");
+            System.out.println("    ◥███████◤");
+            System.out.println("     ══╩══╩══");
+            System.out.println("         |");
+            System.out.println("         |");
+            System.out.println("   ╔+++++++++++++╗");
+            System.out.println("     VOCÊ VENCEU");
+            System.out.println("   ╚+++++++++++++╝");
+            System.err.println("\n");
+            System.err.println("█████████ ");
+            System.err.println("█▄█████▄█");
+            System.err.println("█▼▼▼▼▼" );
+            System.err.println("█   " + "!!!Você ganhou e matou o YanDev!!!");
+            System.err.println("█▲▲▲▲▲");
+            System.err.println("█████████");
+            System.err.println( "██ ██");
+        }
+        else {
+            System.err.println("\n");
+            System.err.println("█████████ ");
+            System.err.println("█▄█████▄█");
+            System.err.println("█▼▼▼▼▼" );
+            System.err.println("█   " + "!!!Você perdeu e matou o YanDev!!!");
+            System.err.println("█▲▲▲▲▲");
+            System.err.println("█████████");
+            System.err.println( "██ ██");
+
+        }
+        ler.close();
     }
 }
-
