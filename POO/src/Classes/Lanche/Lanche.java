96954660b752ba575971e7a455a1ae9e8e38f1aa
felipegetulio <persastons@gmail.com>
2022-06-21 18:26:19 -0300

    21/06/2022

--- POO/src/Classes/Lanche/Lanche.java
+++ POO/src/Classes/Lanche/Lanche.java
@@ -16,6 +16,7 @@ public abstract class Lanche {
             }
         }
     }
+    public abstract void mostrarDetalhesComanda();
 
     public String[] getIngredientes() {
         return ingredientes;
